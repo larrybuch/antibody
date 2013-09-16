27425526e30e26c667b33f1b034e26ddc4de280c
Larry Buchanan <larrybuch@gmail.com>
2013-09-15 22:57:14 -0400

    added posts model

--- app.rb
+++ app.rb
@@ -1,22 +1,41 @@
 require 'sinatra'
 require 'sinatra/activerecord'
-require './config/environments' #database configuration
-require './models/model'        #Model class
- 
-get '/' do
-  erb :index
+require './config/environments' #database config
+require './models/post'         #Post class
+
+get "/" do
+  @posts = Post.order("created_at DESC")
+  @title = "Welcome."
+  erb :"posts/index"
 end
- 
-post '/submit' do
-  @model = Model.new(params[:model])
-  if @model.save
-    redirect '/models'
+
+get "/posts/create" do
+  @title = "Create post"
+  @post = Post.new
+  erb :"posts/create"
+end
+
+post "/posts" do
+  @post = Post.new(params[:post])
+  if @post.save
+    redirect "posts/#{@post.id}"
   else
-    "Sorry, there was an error!"
+    erb :"posts/create"
   end
 end
- 
-get '/models' do
-  @models = Model.all
-  erb :models
+
+get "/posts/:id" do
+  @post = Post.find(params[:id])
+  @title = @post.title
+  erb :"posts/show"
+end
+
+helpers do
+  def title
+    if @title
+      "#{@title}"
+    else
+      "Welcome."
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130915040236) do
+ActiveRecord::Schema.define(version: 20130916022142) do
 
   create_table "models", force: true do |t|
     t.string "name"
+  end
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
